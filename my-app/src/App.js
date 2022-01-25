5a40d63b4e8248284b58ab04fd97e2fc4ab38ff2
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-01-25 17:33:04 +0100

    integration elements Header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,19 +9,20 @@ import Housing from './components/Housing/Housing'
 import About from './components/About/About'
 import Error404 from './components/Error404/Error404'
 
+
 function App() {
   return (
-    <div className="App">
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/logement/:id" element={<Housing />}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/404" element={<Error404 />}/>
-    </Routes>
-    <Footer />
-    </div>
-  )
+      <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/logement/:id" element={<Housing />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/404" element={<Error404 />}/>
+      </Routes>
+      <Footer />
+      </div>
+  );
 }
 
-export default App;
+export default App
--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import './Header.scss'
+import Logo from '../Logo/Logo'
+
 export default function Header() {
     return (
-        <div>
-            <p>test Header</p>
+        <div className='banniere'>
+            <Logo />
+
+            {/* transformer par un composant Navigation ? */}
+            <nav>
+                <ul className='navigation'>
+                    <li>Accueil</li>
+                    <li>À propos</li>
+                </ul>
+            </nav>
         </div>
     )
 }

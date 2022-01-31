fd6879f983fe8c669876bd1b32b94d653640a8a3
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-01-31 12:17:24 +0100

    deploy to GitHub Pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 import './index.scss'
 import App from './App'
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 )
 
